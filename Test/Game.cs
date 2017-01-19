9c040420544a6694400743021af110994d1445da
Mikey <mikeysimpson4@gmail.com>
2017-01-18 17:34:13 -0800

    Initial Commit with working code

---
+++ Test/Game.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Window;
+using SFML.Graphics;
+
+namespace Test
+{
+    abstract class Game
+    {
+        protected RenderWindow window;
+        protected Color clearColor;
+
+        public Game(uint width, uint height, string title, Color clearColor)
+        {
+            this.window = new RenderWindow(new VideoMode(width, height), title, Styles.Close);
+            this.clearColor = clearColor;
+
+            // Set-up Events
+            window.Closed += onClosed;
+            window.KeyPressed += onKeyPressed;
+        }
+
+        public void Run()
+        {
+            LoadContent();
+            Initialize();
+
+            while (window.IsOpen)
+            {
+                window.DispatchEvents();
+                Update();
+
+                window.Clear(clearColor);
+                Draw();
+                window.Display();
+            }
+        }
+
+        protected abstract void LoadContent();
+        protected abstract void Initialize();
+
+        protected abstract void Update();
+        protected abstract void Draw();
+
+        private void onClosed(object sender, EventArgs e)
+        {
+            window.Close();
+        }
+
+        private void onKeyPressed(object sender, KeyEventArgs e)
+        {
+            Console.WriteLine(e.Code);
+        }
+    }
+}
---
+++ Test/SA.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace Test
+{
+    class SA : Game
+    {
+        Dictionary<SFML.Window.Keyboard.Key, bool[]> keys = new Dictionary<SFML.Window.Keyboard.Key, bool[]>();
+        public SA() : base(800, 600, "Say Again?", Color.Magenta)
+        {
+            window.KeyPressed += onKeyPressed;
+            window.KeyReleased += onKeyReleased;
+        }
+
+        private void onKeyReleased(object sender, KeyEventArgs e)
+        {
+            keys[e.Code] = new bool[] { false, e.Shift, e.Control, e.Alt };
+        }
+
+        private void onKeyPressed(object sender, KeyEventArgs e)
+        {
+            if (!keys.ContainsKey(e.Code))
+            {
+                keys.Add(e.Code, new bool[] { true, e.Shift, e.Control, e.Alt });
+            } else
+            {
+                keys[e.Code] = new bool[] { true, e.Shift, e.Control, e.Alt };
+            }
+        }
+
+        protected override void LoadContent()
+        {
+            
+        }
+
+        protected override void Initialize()
+        {
+            
+        }
+
+        protected override void Update()
+        {
+
+        }
+
+        protected override void Draw()
+        {
+            
+        }
+    }
+}
