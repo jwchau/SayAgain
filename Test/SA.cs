f93ba6045e9b4cbe90809804dfc6165adb9d1679
jillzy <jillzyeung@gmail.com>
2017-02-08 19:02:05 -0800

    temp master

--- Test/SA.cs
+++ Test/SA.cs
@@ -9,9 +9,11 @@ using SFML.Window;
 using SFML.System;
 using System.Drawing;
 
-namespace Test {
-    
-    class SA : Game {
+namespace Test
+{
+
+    class SA : Game
+    {
 
 
         public View fullScreenView, charView;
@@ -19,7 +21,8 @@ namespace Test {
         private CharacterState Alex = new CharacterState(4.0, 6.9);
 
 
-        public SA() : base(VideoMode.DesktopMode.Width, VideoMode.DesktopMode.Height, "Say Again?", Color.Magenta) {
+        public SA() : base(VideoMode.DesktopMode.Width, VideoMode.DesktopMode.Height, "Say Again?", Color.Magenta)
+        {
 
             window.KeyPressed += onKeyPressed;
             window.KeyReleased += onKeyReleased;
@@ -29,22 +32,27 @@ namespace Test {
 
         }
 
-        private void onMouseMoved(object sender, MouseMoveEventArgs e) {
-            ManagerOfInput.MouseMoveCheck(State.GetState(),e.X,e.Y);
+        private void onMouseMoved(object sender, MouseMoveEventArgs e)
+        {
+            ManagerOfInput.MouseMoveCheck(State.GetState(), e.X, e.Y);
         }
 
-        private void onMouseButtonReleased(object sender, MouseButtonEventArgs e) {
-            ManagerOfInput.MouseReleasedCheck(State.GetState(), ui_man, tfx,cf);
+        private void onMouseButtonReleased(object sender, MouseButtonEventArgs e)
+        {
+            ManagerOfInput.MouseReleasedCheck(State.GetState(), ui_man, tfx, cf);
         }
 
-        private void onMouseButtonPressed(object sender, MouseButtonEventArgs e) {
+        private void onMouseButtonPressed(object sender, MouseButtonEventArgs e)
+        {
             ManagerOfInput.MouseClickedCheck(State, ui_man, startMenu, pauseMenu, settingsMenu, e.X, e.Y);
         }
 
-        private void onKeyReleased(object sender, KeyEventArgs e) {
+        private void onKeyReleased(object sender, KeyEventArgs e)
+        {
         }
 
-        private void onKeyPressed(object sender, KeyEventArgs e) {
+        private void onKeyPressed(object sender, KeyEventArgs e)
+        {
 
 
             if (e.Code == Keyboard.Key.Space)
@@ -52,17 +60,14 @@ namespace Test {
                 ui_man.SetPrintTime(0);
             }
 
-            if (e.Code == Keyboard.Key.Space)
+            if (e.Code == Keyboard.Key.N)
             {
-                ui_man.SetPrintTime(0);
-            }
-
-            if (e.Code == Keyboard.Key.N) {
-                ui_man.DialogueNextEndCheck();
+                ui_man.DialogueNext();
 
             }
 
-            if (e.Code == Keyboard.Key.P) {
+            if (e.Code == Keyboard.Key.P)
+            {
                 ManagerOfInput.PKeyCheck(State);
             }
 
@@ -72,7 +77,7 @@ namespace Test {
             {
                 //init = true;
                 ui_man.setDialogueBoxPos(new FloatRect(0.0f, 0f, 0.35f, 0.2f));
-                ui_man.RenderDialogue("whos ur daddy im ur daddy whos ur daddy im ur daddy "+
+                ui_man.RenderDialogue("whos ur daddy im ur daddy whos ur daddy im ur daddy " +
                     "whos ur daddy im ur daddy whos ur daddy im ur daddy " +
                     "whos ur daddy im ur daddy whos ur daddy im ur daddy " +
                     "whos ur daddy im ur daddy whos ur daddy im ur daddy ", "Dad");
@@ -80,7 +85,8 @@ namespace Test {
 
 
 
-            if (e.Code == Keyboard.Key.A) {
+            if (e.Code == Keyboard.Key.A)
+            {
                 ui_man.setDialogueBoxPos(new FloatRect(0.3f, 0f, 0.35f, 0.2f));
                 ui_man.RenderDialogue("im alexis im alexis im alexis im alexis im alexis im alexis im alexis im alexis im alexis im alexis im alexis im alexis im alexis im alexis im alexis im alexis im alexis im alexis im alexis ", "Alex");
             }
@@ -92,12 +98,12 @@ namespace Test {
                 ui_man.RenderDialogue("mushroom mom mushroom mom mushroom mom mushroom mom mushroom mom mushroom mom mushroom mom mushroom mom mushroom mom ", "Mom");
             }
 
->>>>>>> 2a2addf2f1e476faa5bc0e61d556e2bc4807a492
         }
 
 
 
-        protected override void LoadContent() {
+        protected override void LoadContent()
+        {
             double[] nums = { -1, 2, 3, 4,
                                1, 2, 3, 4,
                                1, 2, 3, 4, };
@@ -109,37 +115,8 @@ namespace Test {
 
         }
 
-        protected override void Initialize() {
-<<<<<<< HEAD
-            Texture texture;
-            FileStream f = new FileStream("../../Art/angrymom.png", FileMode.Open);
-            texture = new Texture(f);
-
-
-            //mom = new Sprite(texture);
-
-            //the view of the whole game
-            //var temp1 = window.DefaultView;
-            fullScreenView = window.DefaultView;
-            fullScreenView.Viewport = new FloatRect(0, 0, 1, 1);
-            window.SetView(fullScreenView);
-            //the view port is the whole window
-
-            //fullScreenView.Viewport = new FloatRect(0, 0, 1, 1);
-            //window.SetView(fullScreenView);
-            //dialogueBox = new DialogueBox(0, 0, 710, 150);
-            ui_man.setDialogueBox();
-
-
-            //charView = new View(mom.GetGlobalBounds());
-            //charView.Viewport = new FloatRect(0.7f, 0.3f, 0.23f, 0.5f);
-
-/*master =======
-            temp1.Viewport = new FloatRect(0, 0, 1, 1);
-            window.SetView(temp1);
-            ui_man.setDialogueBox();// = new DialogueBox(0, 0, 710, 150);
-            
-=======
+        protected override void Initialize()
+        {
             /*Texture texture;
             FileStream f = new FileStream("../../Art/angrymom.png", FileMode.Open);
             texture = new Texture(f);*/
@@ -149,43 +126,19 @@ namespace Test {
             ui_man.setDialogueBox();
             ui_man.setViews(fullScreenView);
         }
->>>>>>> 2a2addf2f1e476faa5bc0e61d556e2bc4807a492
 
         protected override void Update()
         {
             ui_man.UpdateTimer(State, ManagerOfInput);
 
-*/
 
-/*
-    
-
-
-    */
-
-
-<<<<<<< HEAD
-            ui_man.Icantevenwiththiscode3(State, ManagerOfInput);
         }
 
-        protected override void Update()
+        protected override void Draw()
         {
-            if (ui_man.getDialogueBox().active)
-            {
-                //UNCOMMENT
-                window.Draw(ui_man.getDialogueBox());
-            }
-            
-        }
-=======
-        }
->>>>>>> 2a2addf2f1e476faa5bc0e61d556e2bc4807a492
-            
-        protected override void Draw() {
             window.Clear(Color.Magenta);
             ui_man.DrawDialogueBox(window);
             ui_man.DrawUI(window, State, ui_man, startMenu, pauseMenu, settingsMenu);
-
 
 
         }
