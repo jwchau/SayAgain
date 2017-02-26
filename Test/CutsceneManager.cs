aebff3745e5e5eb0fe92826bf92fe6b336b78db2
jillzy <jillzyeung@gmail.com>
2017-02-26 14:36:10 -0800

    changes

--- Test/CutsceneManager.cs
+++ Test/CutsceneManager.cs
@@ -13,7 +13,7 @@ namespace Test
 {
     class CutsceneManager
     {
-        public Sprite mom;
+        public Sprite momAng, momHap;
 
         public View view { get; private set; }
 
@@ -24,40 +24,49 @@ namespace Test
     {
         int index = 0;
         List<Sprite> sprites = new List<Sprite>();
+        List<Sprite> spritesH = new List<Sprite>(); //happy
         DateTime time = DateTime.Now;
 
         public void Draw(RenderTarget target, RenderStates states)
         {
-            
-        
+
+
             target.Draw(sprites[index]);
 
             float framerate = 4f;
 
-            if ((DateTime.Now - time).TotalMilliseconds > (1000f / framerate))
+            if ((DateTime.Now - time).TotalMilliseconds > (1400f / framerate))
             {
                 time = DateTime.Now;
                 if (++index >= sprites.Count)
                 {
-                    index = 0;               
+                    index = 0;
                 }
             }
-            
+
         }
 
         public Mom()
         {
             int w = 361;
             IntRect first = new IntRect(0, 0, w, 450);
-            FileStream f = new FileStream("../../Art/mom_angry.png", FileMode.Open);
+            FileStream f = new FileStream("../../Art/momsprites.png", FileMode.Open);
             Texture t = new Texture(f);
             f.Close();
-            for (int i = 0; i < t.Size.X; i += w)
+            for (int i = 0; i < (361 * 7); i += w)
             {
-                sprites.Add(new Sprite(t, new IntRect(i, 0, w, 450))); //btw might get extra sprite if sizes no precise
+                sprites.Add(new Sprite(t, new IntRect(i, 0, w, 465))); //btw might get extra sprite if sizes no precise
             }
-      
+            for (int i = 0; i < (361 * 9); i += w)
+            {
+                spritesH.Add(new Sprite(t, new IntRect(i, 465, w, 465))); //second row of sprites; happy epression 
+            }
+
+
         }
+
+
+
     }
 
 
