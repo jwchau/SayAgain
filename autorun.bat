73d18eb936fae31efdf0cffb6e86772c9c89eed3
jwchau <jwchau@ucsc.edu>
2017-06-02 10:49:49 -0700

    fix where installer gets put

--- autorun.bat
+++ autorun.bat
@@ -7,7 +7,6 @@ robocopy .\Test\Art ..\SayAgainGame\content\Art /E
 robocopy .\Test\Fonts ..\SayAgainGame\content\Fonts /E
 robocopy .\Test\Sounds ..\SayAgainGame\content\Sounds /E
 robocopy .\Test\ ..\SayAgainGame\content *.json /E
-Xcopy .\installer.bat ..\SayAgainGame
-
+Xcopy .\installer.bat %userprofile%\Desktop\
 exit
 
