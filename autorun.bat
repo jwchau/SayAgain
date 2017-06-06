f7043ea14026183d84f65a36c3b9f839d7ac2c3e
john chau <jwchau@ucsc.edu>
2017-06-06 10:41:17 -0700

    fixed installer, works from anywhere

--- autorun.bat
+++ autorun.bat
@@ -7,7 +7,7 @@ robocopy .\Test\Art ..\SayAgainGame\content\Art /E
 robocopy .\Test\Fonts ..\SayAgainGame\content\Fonts /E
 robocopy .\Test\Sounds ..\SayAgainGame\content\Sounds /E
 robocopy .\Test\ ..\SayAgainGame\content *.json /E
-Xcopy .\installer.bat %userprofile%\Desktop\
+Xcopy .\installer.bat %userprofile%\Desktop\SayAgainGame
 
 exit
 
--- installer.bat
+++ installer.bat
@@ -3,11 +3,11 @@
 set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
 
 echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
-echo sLinkFile = "%USERPROFILE%\Desktop\SA.lnk" >> %SCRIPT%
+echo sLinkFile = "%~dp0\SA.lnk" >> %SCRIPT%
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
-echo oLink.TargetPath = "%userprofile%\Desktop\SayAgainGame\content\bin\debug\SayAgain.exe" >> %SCRIPT%
-echo oLink.IconLocation = "%userprofile%\Desktop\SayAgainGame\content\Art\exe_icon.ico" >> %SCRIPT%
-echo oLink.WorkingDirectory = "%userprofile%\Desktop\SayAgainGame\content\bin\debug" >> %SCRIPT%
+echo oLink.TargetPath = "%~dp0\content\bin\debug\SayAgain.exe" >> %SCRIPT%
+echo oLink.IconLocation = "%~dp0\content\Art\exe_icon.ico" >> %SCRIPT%
+echo oLink.WorkingDirectory = "%~dp0\content\bin\debug" >> %SCRIPT%
 echo oLink.Save >> %SCRIPT%
 
 cscript /nologo %SCRIPT%
